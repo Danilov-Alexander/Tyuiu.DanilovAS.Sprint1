2142b142fc0674d86e53801183f459aa2a56b6b4
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2023-09-18 12:49:39 +0500

    Task0

---
+++ Tyuiu.DanilovAS.Sprint1.Task0.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DanilovAS.Sprint1.Task0.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DanilovAS.Sprint1.Task0.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0d22c3b-9082-467c-873d-12b9eb781227")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DanilovAS.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DanilovAS.Sprint1.Task0.V3.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint1.Task0.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Данилов А. С. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИСТНб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (6/2*3)/9 и             *");
+            Console.WriteLine("* печатает результат на экране.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (6/2*3)/9                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+            
+        }
+    }
+}
