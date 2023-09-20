dfc28206f390fae1f60fec6e519a2632a36fd3db
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2023-09-20 21:22:08 +0500

    Task2

---
+++ Tyuiu.DanilovAS.Sprint1.Task2.V13.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DanilovAS.Sprint1.Task2.V13.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint1.Task2.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            // 1 миля = 1.609 км
+            // 1.609
+            // 12 миль = 19.308 км
+
+            DataService ds = new DataService();
+            int x = 12;
+            var res = ds.ConvertMilesToKm(x);
+            Assert.AreEqual(19.308, res);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint1.Task2.V13/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DanilovAS.Sprint1.Task2.V13.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint1.Task2.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Данилов А. С. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИСТНб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("* Перевести ведённые пользователем мили в км.                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите кол-во миль, чтобы узнать кол-во км: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Ответ: " + ds.ConvertMilesToKm(x) + " км.");
+            Console.ReadKey();
+        }
+    }
+}
