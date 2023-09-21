a9d3d984646083b409b9b5612ba9df47a74f33f9
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2023-09-21 21:56:47 +0500

    Task3

--- Tyuiu.DanilovAS.Sprint1.Task3.V8.Test/DataServiceTest.cs
+++ Tyuiu.DanilovAS.Sprint1.Task3.V8.Test/DataServiceTest.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.DanilovAS.Sprint1.Task3.V8.Test
         {
             DataService ds = new DataService();
             double timeDays = 30; // Срок вклада
-            double percent = 20; // Проц.Ставка
+            double percent = 20; // Проц. Ставка
             double wait = 2541.10; // Переменная, которую мы ожидаем получить в методе AreEqual
             var res = Math.Round(ds.IncomeAmount(percent,timeDays),2);
             Assert.AreEqual(wait, res);
